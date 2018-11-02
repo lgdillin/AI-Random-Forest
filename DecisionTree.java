dfc6f3a5494ecb44cb559a128cda7434cff9c126
Liam Dillingham <mailgd9@gmail.com>
2018-11-02 14:32:13 -0500

    some work

--- DecisionTree.java
+++ DecisionTree.java
@@ -11,27 +11,72 @@ class InteriorNode extends Node {
   Node a;
   Node b;
 
+  InteriorNode() {
+
+  }
+
   boolean isLeaf() { return false; }
 }
 
 class LeafNode extends Node {
   double[] label;
 
-  boolean isLeaf() { return true; } 
+  LeafNode() {
+
+  }
+
+  boolean isLeaf() { return true; }
 }
 
 class DecisionTree extends SupervisedLearner {
-  String name() {
-    return "";
+  Node root;
+
+  DecisionTree() {
+
+  }
+
+  String name() { return ""; }
+
+  Node buildTree(Matrix features, Matrix labels) {
+    if(features.rows() != labels.rows()) throw new RuntimeException("Rows unequal");
+
+    int col = pickDividingColumn(features, labels);
+    double pivot = pickPivot(features, labels);
+
+    int vals = features.valueCount(col);
+
+    // copies the meta-data
+    Matrix featA = new Matrix(features);
+    Matrix featB = new Matrix(features);
+    Matrix labelA = new Matrix(labels);
+    Matrix labelB = new Matrix(labels);
+
+    for(int i = 0; i < features.rows(); ++i) {
+
+      // Data is continuous
+      if(vals == 0) {
+
+        // Decide if the data is greater than or less than pivot
+        if(features.row(i)[col] < pivot) {
+          featA.takeRow(features.removeRow(i));
+          labelA.takeRow(labels.removeRow(i));
+        } else {
+          featB.takeRow(features.removeRow(i));
+          labelB.takeRow(labels.removeRow(i));
+        }
+
+      // Data is non-continuous
+      } else {
+
+      }
+    }
   }
 
   void train(Matrix features, Matrix labels) {
-
+    root = buildTree(features, labels);
   }
 
   void predict(double[] in, double[] out) {
 
   }
-
-  Node root;
 }
